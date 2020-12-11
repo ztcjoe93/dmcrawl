81076e739a861cf0bfe4716ad504d6087623b1be
zt <ztcjoe93@gmail.com>
2020-12-11 12:54:23 +0800

    sites.txt for optimized parsing

--- src/CrawlerOptions.java
+++ src/CrawlerOptions.java
@@ -1,7 +1,34 @@
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
+
 public class CrawlerOptions {
     String language = "en";
+    HashSet<String> urlSet = new HashSet<String>();
 
-    public CrawlerOptions() {}
+    public CrawlerOptions() {
+       File urlList = new File("sites.txt"); 
+
+       if (urlList.exists()){
+           try (BufferedReader reader = Files.newBufferedReader(Paths.get("sites.txt"))){
+               String line;
+               while ((line = reader.readLine()) != null) {
+                   urlSet.add(line);
+               }
+           } catch (Exception e){
+               e.printStackTrace();
+           }
+       } else {
+           try {
+               urlList.createNewFile();
+           } catch (Exception e){
+               e.printStackTrace();
+           }
+       }
+    }
 
     void toggleLanguage() {
         if(this.language == "en"){
